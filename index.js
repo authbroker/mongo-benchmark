ca80140b7a34f49a619673b2f7be99e1eaac8a13
Hadi Mahdavi <expandboard@users.noreply.github.com>
2019-06-26 20:32:34 +0430

    Remove comments

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ var debug = require('debug')
 var mongo = require('./lib/mongo')
 var validData = require('./lib/validDocs')
 
-/*
-// we create clients tables in auth collection and paraffin database
-var url = process.env.DB_AUTH_URL || 'mongodb://localhost:27017'
-var collectionName = DB_AUTH_COLLECTION || 'authBroker'
-const methodology = process.env.METHODOLOGY || 'vertical'
-*/
-
 
 function mongoBenchmark(opts) {
     //this.opts = opts
