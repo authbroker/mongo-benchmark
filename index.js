5b00d8b103d876b0dc358cc65ebdc17663ac4e88
Hadi Mahdavi <expandboard@users.noreply.github.com>
2019-06-27 16:08:41 +0430

    Second commit

--- index.js
+++ index.js
@@ -2,30 +2,28 @@ var debug = require('debug')
 var mongo = require('./lib/mongo')
 var validData = require('./lib/validDocs')
 
-
 function mongoBenchmark(opts) {
-    //this.opts = opts
+    this.opts = opts
     this.mongo = new mongo(opts.db)
 }
-
 
 mongoBenchmark.prototype.insertRandomData = function (recordsNo) {
     saving(docs)
 }
 
-
 mongoBenchmark.prototype.insertValidData = function () {
-    this.mongo.saving(validData)
+    this.mongo.saving(validData(this.opts.salt))
 }
 
-
+mongoBenchmark.prototype.validData = function () {
+    return validData(this.opts.salt)
+}
 
 mongoBenchmark.prototype.getValidData = function (clientId) {
-    for (i = 0; i < validData.length; i++)
-        if (validData[i].clientId === clientId)
-            return validData[i]
+    var data = validData(this.opts.salt)
+    for (i = 0; i < data.length; i++)
+        if (data[i].clientId === clientId)
+            return data[i]
 }
-
-
 
 module.exports = mongoBenchmark
