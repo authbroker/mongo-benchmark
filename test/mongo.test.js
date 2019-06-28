7620352fe04e1e4a4fe2b543a6bedcc6355f60d4
Hadi Mahdavi <expandboard@users.noreply.github.com>
2019-06-28 19:39:06 +0430

    Edit opts

--- test/mongo.test.js
+++ test/mongo.test.js
@@ -2,12 +2,17 @@ var test = require('../index')
 
 var opts = {
     db: {
-        url: 'mongodb://localhost:27017/paraffin',
-        collectionName: 'authBroker',
-        methodology: 'vertical'
+      url: 'mongodb://localhost:27017/paraffin',
+      collectionName: 'authBroker',
+      methodology: 'vertical'
+    },
+    salt: {
+      iterations: 10,
+      hashBytes: 64,
+      digest: 'sha512',
+      salt: 'salt'
     }
-}
-
+  }
 console.log('Test is starting...')
 
 var runTest = new test(opts)
