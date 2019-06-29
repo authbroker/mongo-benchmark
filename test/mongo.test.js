e767b3627d540e02b637df6f00f8b7c3a31b9838
Hadi Mahdavi <expandboard@users.noreply.github.com>
2019-06-29 07:24:46 +0430

    Add a test

--- test/mongo.test.js
+++ test/mongo.test.js
@@ -1,19 +1,33 @@
+var assert = require('assert')
 var test = require('../index')
 
 var opts = {
-    db: {
-      url: 'mongodb://localhost:27017/paraffin',
-      collectionName: 'authBroker',
-      methodology: 'vertical'
-    },
-    salt: {
-      iterations: 10,
-      hashBytes: 64,
-      digest: 'sha512',
-      salt: 'salt'
-    }
+  db: {
+    url: 'mongodb://localhost:27017/paraffin',
+    collectionName: 'authBroker',
+    methodology: 'vertical'
+  },
+  salt: {
+    iterations: 10,
+    hashBytes: 64,
+    digest: 'sha512',
+    salt: 'salt'
   }
-console.log('Test is starting...')
+}
 
-var runTest = new test(opts)
-runTest.insertValidData()
+describe('Benchmark Verification', function () {
+  var runTest = new test(opts)
+  var validData
+
+  describe('validData function', function () {
+
+    beforeEach(function (done) {
+      runTest = new test(opts)
+      validData = runTest.validData
+    })
+
+    it('should return clientID when call validData function', function () {
+      assert.equal('0050bdee-dd8b-43a3-8602-a10f1d0e2659', validData[0].clientId)
+    })
+  })
+})
