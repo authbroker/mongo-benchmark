d7bdd7dbf56fe3f63b042f90719c51001913e7ca
Hadi Mahdavi <expandboard@users.noreply.github.com>
2019-07-08 13:22:47 +0430

    Add log

--- test/mongo.test.js
+++ test/mongo.test.js
@@ -24,16 +24,19 @@ describe('Benchmark Verification', function () {
     beforeEach(function (done) {
       runTest = new test(opts)
       validData = runTest.validData()
+      console.log(validData)
     })
 
     it('should return client setting when call getValidData function', function () {
       //assert.equal(validData[1], runTest.getValidData(validData[1].clientId))
-      expect(validData[1]).to.eql(nTest.getValidData(validData[1].clientId))
+      console.log(runTest.getValidData(validData[1].clientId))
+      expect(validData[1]).to.eql(runTest.getValidData(validData[1].clientId))
       done()
     })
 
     it('should return client setting when call readData function', function () {
       //assert.equal(validData[1], runTest.readData(validData[1].clientId))
+      console.log(runTest.readData(validData[1].clientId))
       expert(validData[1]).to.eql(runTest.readData(validData[1].clientId))
       done()
     })
