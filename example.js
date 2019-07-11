277ba3b3add2e99b5bf98caacb5cf0271b8624a5
Hadi Mahdavi <expandboard@users.noreply.github.com>
2019-07-11 15:00:31 +0430

    Add example

--- example.js
+++ example.js
@@ -1,23 +1,30 @@
 var benchmark = require('@authbroker/mongo-benchmark')
 
 var opts = {
-  db: {
-    url: 'mongodb://localhost:27017/paraffin',
-    collectionName: 'authBroker',
-    methodology: 'vertical'
-  },
-  salt: {
-    iterations: 10,
-    hashBytes: 64,
-    digest: 'sha512',
-    salt: 'salt'
-  }
+    db: {
+        url: 'mongodb://localhost:27017/paraffin',
+        collectionName: 'authBroker',
+        methodology: 'vertical'
+    },
+    salt: {
+        iterations: 10,
+        hashBytes: 64,
+        digest: 'sha512',
+        salt: 'salt'
+    }
 
 }
 var demo = new benchmark(opts)
 
 // print valid demo data list
-console.log(demo.validData())
+console.log('print valid demo data list:')
+var validData = demo.validData()
+console.log(validData)
 
 // save valid demo data to db
-demo.insertValidData()
+demo.insertValidData(function () {
+    demo.readData(validData[1].clientId, function (callback) {
+        console.log('clientID -> ' + validData[1].clientId + ' is searching in db:')
+        console.log(callback)
+    })
+})
