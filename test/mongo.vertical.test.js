0da81c06b7ac6839b6c12fc2fa9118de95ca8ed2
Hadi Mahdavi <expandboard@users.noreply.github.com>
2019-07-14 12:53:59 +0430

    Fix bug

---
+++ test/mongo.vertical.test.js
@@ -0,0 +1,79 @@
+var assert = require('assert')
+var test = require('../index')
+
+
+describe('Benchmark Verification', function () {
+    var validData
+    var runTest
+    var opts
+
+    describe('vertical architecture', function () {
+
+
+        before(function (done) {
+            opts = {
+                db: {
+                    url: 'mongodb://localhost:27017/paraffin',
+                    dbName: 'paraffin',
+                    collectionName: 'authBroker',
+                    methodology: 'vertical'
+                },
+                salt: {
+                    iterations: 10,
+                    hashBytes: 64,
+                    digest: 'sha512',
+                    salt: 'salt'
+                }
+            }
+
+            runTest = new test(opts)
+            done()
+        })
+
+        it('should return client setting when call getValidData function', function () {
+            validData = runTest.validData()
+            var actual = runTest.getValidData(validData[1].clientId)
+            assert(validData[1].clientId === actual.clientId, 'Valid Data is valid!')
+        })
+
+
+        /*
+        it('Should return true if insert valid data', function () {
+            runTest.insertValidData(function (callback) {
+                assert(callback.result.ok, 'true is expected.')
+            })
+        })
+*/
+
+        it('Should return true if insert valid data', function () {
+            runTest.insertValidData(function (result) {
+                //console.log(result)
+                runTest.readData({clientId: validData[1].clientId, realm:validData[1].realm }, function (callback) {
+                    //console.log(callback)
+                    assert(validData[1].clientId === callback.clientId, 'Read data is invalid!')
+                })
+            })
+        })
+
+
+
+        /*
+        it('should return client setting when call readData function', function () {
+            runTest.readData(validData[1].clientId, function (callback) {
+                assert(validData[1].clientId === callback.clientId, 'Read data is invalid!')
+            })
+        })
+
+
+        it('Should return true if drop collection is done', function () {
+            runTest.drop(opts.db.collectionName, function (callback) {
+                assert(callback, 'true is expected.')
+            })
+        })
+
+        */
+       
+    })
+
+
+})
